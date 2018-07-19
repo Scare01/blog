635167cc53a73483a2cba74d09eebd80bdadd039
Aleksei Slesarev <lexsl84@gmail.com>
2018-07-19 13:13:39 +0700

    migrate db

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=500)
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    modified_at = models.DateTimeFiels(auto_now=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
