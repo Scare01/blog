c40f8fe8b746550f2e315edfac5fd0f611a5c129
Aleksei Slesarev <lexsl84@gmail.com>
2018-07-19 11:52:49 +0700

    crate 2 test for index page

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from posts.views import index
 
-# Create your tests here.
+
+class IndexPageTest(TestCase):
+
+    def test_root_url_resolves_to_index_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_index_page_returns_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue("<title>Helpitessa's Blog", html)
+        self.assertTrue(html.endswith('</html>'))
