cbeebeecabb01084de1b954813a9911507d2b815
Aleksei Slesarev <aleksei@Mac-mini-Aleksei.local>
2018-07-21 17:53:38 +0700

    create posts and tests

--- posts/tests.py
+++ posts/tests.py
@@ -11,18 +11,26 @@ class IndexPageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, index)
 
-    def test_index_page_returns_html(self):
-        request = HttpRequest()
-        response = index(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertTrue("<title>Helpitessa's Blog", html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_index(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_wrong_root(self):
+        resp = self.client.get('/wrong')
+        self.assertEqual(resp.status_code, 404)
 
     # def test_index_page_display_all_posts(self):
 
 
+class PostPageTest(TestCase):
+    def test_post_detail_page(self):
+        posts = Post.objects.all()
+        post = Post.objects.create(title='First post')
+        self.assertEqual(posts[0].title, 'First post')
+
+
 class PostModelTest(TestCase):
     def test_string_representation(self):
-        post = Post(title="My post title")
+        post = Post.objects.create(title="My post title")
         self.assertEqual(str(post), post.title)
+        self.assertEqual(post.id, 1)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/index.html', context)
+
+# def detail(request):
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts/detail.html', {'post': post})
