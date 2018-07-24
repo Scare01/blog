7ec41ab7f8edca6aca02bc5b48d80a6b6786b679
Aleksei Slesarev <lexsl84@gmail.com>
2018-07-24 13:56:59 +0700

    add image to posts

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.conf import settings
+from django.utils.safestring import mark_safe
 
 
 class Post(models.Model):
     title = models.CharField(max_length=500)
     body = models.TextField()
+    image = models.ImageField(upload_to='media')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
